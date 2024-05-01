9361246a329ff0e71476f966b11438ce83a438e4
Ankur Roy <ankurroyy0360roy@gmail.com>
2024-05-02 00:32:56 +0530

    insertLast

--- linkedlist/singly.cpp
+++ linkedlist/singly.cpp
@@ -22,6 +22,16 @@ Node *insertBegin(Node *head, int x){
     newNode -> next = head;
     return newNode;
 }
+Node *insertLast(Node *head, int x){
+    Node *newNode = new Node(x);
+    if(head == nullptr)
+        return newNode;
+    Node *curr = head;
+    while (curr -> next != nullptr)
+        curr = curr -> next;
+    curr -> next = newNode;
+    return head;
+}
 int main(){
     Node *head = new Node(10);
     Node *t1 = new Node(20);
@@ -29,6 +39,7 @@ int main(){
     head -> next = t1;
     t1 -> next = t2;
     head = insertBegin(head, 40);
+    insertLast(head, 50);
     printList(head);
     return 0;
 }
